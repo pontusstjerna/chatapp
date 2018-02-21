0693bef9fbc875d8131a04e5b432429cde364a39
FoxHound94 <guslyher@student.gu.se>
2018-02-21 12:57:58 +0100

    first draft of message routes

---
+++ backend/src/routes/message.js
@@ -0,0 +1,46 @@
+import { Router } from 'express';
+import Message from '../models/message';
+
+export const message = () => {
+    let router = new Router();
+    
+    router.get('/',(req, res, next) => {
+        Message.find()
+        .then(docs => {
+            res.json(docs);
+        }))
+        .catch(err => {
+            res.status(500)
+        })
+
+    });
+
+    router.get('/:id',(req, res, next) => {
+        Message.findOne(req.params.id)
+        .then(docs => {
+            res.json(docs);
+   	    })
+    });
+	
+    router.post('/',(req,res,next) =>{
+        let newMessage = new Message(req.body)
+   	    Message.save().then(doc => {
+            res.json(doc);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+    });
+
+    router.delete('/:id', (req, res, next) => {
+        let messageId = req.params.id;
+        Message.findByIdAndRemove(messageId)
+        .then(doc => {
+            res.json(doc);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+    });
+
+    )
