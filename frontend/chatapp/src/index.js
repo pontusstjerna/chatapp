339a8fb586b81bf25cd0d69d83b3c1d94492cded
FoxHound94 <guslyher@student.gu.se>
2018-03-07 19:21:31 +0100

    fixed bug when getting the language settings from the browser

--- frontend/chatapp/src/index.js
+++ frontend/chatapp/src/index.js
@@ -7,16 +7,21 @@ import registerServiceWorker from './registerServiceWorker';
 /* react-intl for language localizaion */
 import { addLocaleData, IntlProvider } from 'react-intl';
 
-/* Import basic support for another locale if needed
-   ('en' is included by default) */
 import messages_sv from './translations/sv.json';
 import locale_sv from 'react-intl/locale-data/sv';
 
+import locale_en from 'react-intl/locale-data/en';
+import messages_en from './translations/en.json';
+
+
+addLocaleData(locale_en);
 addLocaleData(locale_sv);
+
 const messages = {
     'sv': messages_sv,
+	'en': messages_en
 };
-const language = navigator.language;  // language settings
+const language = navigator.language.split(/[-_]/)[0];  // language settings without the region code en-us becomes en
 
 ReactDOM.render(
 <IntlProvider locale={language} messages={messages[language]}>
